0b8e682f0898d0c3d8637f8785e3a3f7875ad7ec
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-10-31 03:04:06 +0900

    Feat: App 초기 레이아웃

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
-import './App.css';
+import { useState } from 'react';
+import ProgressIndicator from './components/ProgressIndicator';
+import QuestionBox from './components/QuestionBox';
 
 function App() {
-  return <div></div>;
+  const questions = [
+    {
+      title: '질문 1',
+      description: '설명 1',
+      type: 'text',
+      required: false,
+      options: {},
+    },
+    {
+      title: '질문 2',
+      description: '설명 2',
+      type: 'text',
+      required: false,
+      options: {},
+    },
+  ];
+
+  const step = 1;
+  const [answers, setAnswers] = useState([]);
+
+  return (
+    <div className="App">
+      <ProgressIndicator />
+      <QuestionBox
+        question={questions[step]}
+        questionsLength={questions.length}
+        step={step}
+        answer={answers[step]}
+        setAnswers={(newAnswer) => {
+          setAnswers((answers) => {
+            const newAnswers = [...answers];
+            newAnswers[step] = newAnswer;
+            return newAnswers;
+          });
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
