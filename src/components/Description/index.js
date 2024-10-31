94613796f0ce36bee8a5beb8612de57bd23272f0
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-10-31 18:29:09 +0900

    Feat: CSS-Description

--- src/components/Description/index.js
+++ src/components/Description/index.js
@@ -1,3 +1,13 @@
-export default function Description({ children }) {
-  return <h4>{children}</h4>;
-}
+import styled from 'styled-components';
+
+const Description = styled.h4`
+  font-size: 18px;
+  line-height: 21px;
+  color: rgba(70, 60, 60, 1);
+  font-weight: normal;
+  margin-top: 16px;
+  margin-bottom: 60px;
+  margin-left: 38px;
+`;
+
+export default Description;
