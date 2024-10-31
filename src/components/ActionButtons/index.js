25bdc2d5e71271ac3cdd0bdee0a774ab65e28227
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-10-31 17:26:00 +0900

    Feat: CSS-Button

--- src/components/ActionButtons/index.js
+++ src/components/ActionButtons/index.js
@@ -1,16 +1,23 @@
 import { useNavigate } from 'react-router-dom';
+import Button from '../Button';
 export default function ActionButtons({ questionLength, step }) {
   const isLast = questionLength - 1 === step;
   const navigate = useNavigate();
   return (
     <div>
       {step === 0 || (
-        <button onClick={() => navigate(`${step - 1}`)}>이전</button>
+        <Button type="SECONDARY" onClick={() => navigate(`${step - 1}`)}>
+          이전
+        </Button>
       )}
       {isLast ? (
-        <button onClick={() => navigate('done')}>제출</button>
+        <Button type="PRIMARY" onClick={() => navigate('done')}>
+          제출
+        </Button>
       ) : (
-        <button onClick={() => navigate(`${step + 1}`)}>다음</button>
+        <Button type="PRIMARY" onClick={() => navigate(`${step + 1}`)}>
+          다음
+        </Button>
       )}
     </div>
   );
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,3 +1,54 @@
-export default function Button() {
-  return <div>Button</div>;
-}
+import styled from 'styled-components';
+import { PRIMARY, SECONDARY, TERTIARY } from '../../constants/color';
+
+const colorMap = {
+  PRIMARY,
+  SECONDARY,
+  TERTIARY,
+};
+
+const Button = styled.button`
+  padding: 16px 24px;
+  width: 200px;
+  color: ${({ type }) => colorMap[type].BUTTON.DEFAULT.COLOR};
+  background-color: ${({ type }) => colorMap[type].BUTTON.DEFAULT.BACKGROUND};
+  border: ${({ type }) =>
+    type === 'TERTIARY'
+      ? `1px solid ${TERTIARY.BUTTON.DEFAULT.COLOR}`
+      : 'none'};
+  border-radius: 4px;
+
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 21px;
+
+  &:hover {
+    color: ${({ type }) => colorMap[type].BUTTON.HOVER.COLOR};
+    background-color: ${({ type }) => colorMap[type].BUTTON.HOVER.BACKGROUND};
+    border: ${({ type }) =>
+      type === 'TERTIARY'
+        ? `1px solid ${TERTIARY.BUTTON.HOVER.COLOR}`
+        : 'none'};
+  }
+
+  &:active {
+    color: ${({ type }) => colorMap[type].BUTTON.PRESSED.COLOR};
+    background-color: ${({ type }) => colorMap[type].BUTTON.PRESSED.BACKGROUND};
+    border: ${({ type }) =>
+      type === 'TERTIARY'
+        ? `1px solid ${TERTIARY.BUTTON.PRESSED.COLOR}`
+        : 'none'};
+  }
+
+  &:disabled {
+    color: ${({ type }) => colorMap[type].BUTTON.DISABLED.COLOR};
+    background-color: ${({ type }) =>
+      colorMap[type].BUTTON.DISABLED.BACKGROUND};
+    border: ${({ type }) =>
+      type === 'TERTIARY'
+        ? `1px solid ${TERTIARY.BUTTON.DISABLED.COLOR}`
+        : 'none'};
+  }
+`;
+
+export default Button;
