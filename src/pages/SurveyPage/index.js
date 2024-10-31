e199adeaec2ffd5ed792e8d401fa20e38b63624a
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-11-01 01:23:03 +0900

    Refactor: surveypage

--- src/pages/SurveyPage/index.js
+++ src/pages/SurveyPage/index.js
@@ -1,62 +1,11 @@
-import { useState } from 'react';
 import ProgressIndicator from '../../components/ProgressIndicator';
 import QuestionBox from '../../components/QuestionBox';
-import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
-
 export default function SurveyPage() {
-  const params = useParams();
-
-  const questions = [
-    {
-      title: '질문 1',
-      description: '설명 1',
-      type: 'text',
-      required: false,
-      options: {
-        placeholder: '입력...',
-      },
-    },
-    {
-      title: '질문 2',
-      description: '설명 2',
-      type: 'textarea',
-      required: false,
-      options: {
-        placeholder: '입력...',
-      },
-    },
-    {
-      title: '질문 3',
-      description: '설명 3',
-      type: 'select',
-      required: false,
-      options: {
-        items: ['답변1', '답변2', '답변3', '답변4', '답변5'],
-      },
-    },
-  ];
-
-  const step = parseInt(params.step);
-
-  const [answers, setAnswers] = useState([]);
-
   return (
     <SurveyPageWrapper>
       <ProgressIndicator />
-      <QuestionBox
-        question={questions[step]}
-        questionsLength={questions.length}
-        step={step}
-        answer={answers[step]}
-        setAnswers={(newAnswer) => {
-          setAnswers((answers) => {
-            const newAnswers = [...answers];
-            newAnswers[step] = newAnswer;
-            return newAnswers;
-          });
-        }}
-      />
+      <QuestionBox />
     </SurveyPageWrapper>
   );
 }
