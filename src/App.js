e43ba5dd8215991c4a5d84f64bc27d580c5c29e6
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-10-31 18:27:38 +0900

    Feat: CSS-App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import { Routes, Route } from 'react-router-dom';
 import SurveyPage from './pages/SurveyPage';
 import CompletionPage from './pages/CompletionPage';
-
+import styled from 'styled-components';
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/survey/:surveyId" element={<SurveyPage />}>
-          <Route path=":step" element={<SurveyPage />} />
-        </Route>
-        <Route path="/done" element={<CompletionPage />} />
-      </Routes>
-    </div>
+    <AppWrapper>
+      <Main>
+        <Routes>
+          <Route path="/survey/:surveyId" element={<SurveyPage />}>
+            <Route path=":step" element={<SurveyPage />} />
+          </Route>
+          <Route path="/done" element={<CompletionPage />} />
+        </Routes>
+      </Main>
+    </AppWrapper>
   );
 }
 
+const AppWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(245, 246, 246, 1);
+`;
+
+const Main = styled.main`
+  width: 700px;
+  min-height: 500px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 16px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.07);
+  padding: 60px;
+  display: flex;
+  box-sizing: border-box;
+`;
 export default App;
