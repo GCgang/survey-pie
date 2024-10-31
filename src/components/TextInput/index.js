74a0b68cf51c68e77e7463caa2528f75eda6aa7d
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-10-31 16:25:59 +0900

    Feat: CSS-TextInput

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,9 +1,27 @@
-export default function TextInput({ answer, setAnswers }) {
+import styled from 'styled-components';
+export default function TextInput({ answer, setAnswers, options }) {
   return (
-    <input
+    <Input
       type="text"
       value={answer}
       onChange={(e) => setAnswers(e.target.value)}
-    ></input>
+      placeholder={options.placeholder}
+    />
   );
 }
+
+const Input = styled.input`
+  border: 1px solid rgba(224, 224, 224, 1);
+
+  border-radius: 5px;
+  box-sizing: border-box;
+  padding: 12px 18px;
+
+  font-size: 18px;
+  line-height: 21px;
+
+  &:focus {
+    outline: none;
+    border: 1px solid rgba(101, 66, 241, 1);
+  }
+`;
