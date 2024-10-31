2358ca886845f1a20bdb7e1d0430dada0b93d21c
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-10-31 18:30:03 +0900

    Feat: CSS-TextArea

--- src/components/TextAreaInput/index.js
+++ src/components/TextAreaInput/index.js
@@ -5,13 +5,13 @@ export default function TextAreaInput({ answer, setAnswers, options }) {
 }
 
 const TextArea = styled.textarea`
+  width: 100%;
   height: 196px;
   resize: none;
   border: 1px solid rgba(224, 224, 224, 1);
   border-radius: 5px;
   box-sizing: border-box;
   padding: 12px 18px;
-
   font-size: 18px;
   line-height: 21px;
 
