cb456b08bde15e7b167a5793e8a316847f4d0b5e
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-10-31 18:28:51 +0900

    Feat: CSS-Title

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,3 +1,22 @@
+import styled from 'styled-components';
+
 export default function Title({ children }) {
-  return <h1>{children}</h1>;
+  return (
+    <TitleWrapper>
+      <span>Q. </span>
+      {children}
+    </TitleWrapper>
+  );
 }
+
+const TitleWrapper = styled.h1`
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  color: rgba(18, 17, 17, 1);
+  margin: 0;
+
+  span {
+    margin-right: 12px;
+  }
+`;
