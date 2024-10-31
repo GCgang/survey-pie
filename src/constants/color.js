fddf77840906998f0ebff3306134071fe5ecdc04
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-10-31 17:25:36 +0900

    Feat: primary, secondary, tertiary button color

---
+++ src/constants/color.js
@@ -0,0 +1,66 @@
+export const PRIMARY = {
+  BUTTON: {
+    DEFAULT: {
+      COLOR: 'rgba(255, 255, 255, 1)',
+      BACKGROUND: 'rgba(101, 66, 241, 1)',
+    },
+    HOVER: {
+      COLOR: 'rgba(255, 255, 255, 1)',
+      BACKGROUND: 'rgba(153, 130, 244, 1)',
+    },
+    PRESSED: {
+      COLOR: 'rgba(255, 255, 255, 1)',
+      BACKGROUND: 'rgba(75, 49, 176, 1)',
+    },
+    DISABLED: {
+      COLOR: 'rgba(208, 205, 205, 1)',
+      BACKGROUND: 'rgba(237, 237, 237, 1)',
+    },
+  },
+};
+
+export const SECONDARY = {
+  BUTTON: {
+    DEFAULT: {
+      COLOR: 'rgba(58, 58, 58, 1)',
+      BACKGROUND: 'rgba(222, 222, 222, 1)',
+    },
+    HOVER: {
+      COLOR: 'rgba(58, 58, 58, 1)',
+      BACKGROUND: 'rgba(243, 243, 243, 1)',
+    },
+    PRESSED: {
+      COLOR: 'rgba(58, 58, 58, 1)',
+      BACKGROUND: 'rgba(184, 183, 185, 1)',
+    },
+    DISABLED: {
+      COLOR: 'rgba(208, 205, 205, 1)',
+      BACKGROUND: 'rgba(237, 237, 237, 1)',
+    },
+  },
+};
+
+export const TERTIARY = {
+  BUTTON: {
+    DEFAULT: {
+      COLOR: 'rgba(101, 66, 241, 1)',
+      BACKGROUND: 'rgba(255, 255, 255, 1)',
+      BORDER: 'rgba(101, 66, 241, 1)',
+    },
+    HOVER: {
+      COLOR: 'rgba(153, 130, 244, 1)',
+      BACKGROUND: 'rgba(255, 255, 255, 1)',
+      BORDER: 'rgba(101, 66, 241, 1)',
+    },
+    PRESSED: {
+      COLOR: 'rgba(75, 49, 176, 1)',
+      BACKGROUND: 'rgba(240, 237, 252, 1)',
+      BORDER: 'rgba(101, 66, 241, 1)',
+    },
+    DISABLED: {
+      COLOR: 'rgba(208, 205, 205, 1)',
+      BACKGROUND: 'rgba(246, 246, 246, 1)',
+      BORDER: 'rgba(237, 237, 237, 1)',
+    },
+  },
+};
